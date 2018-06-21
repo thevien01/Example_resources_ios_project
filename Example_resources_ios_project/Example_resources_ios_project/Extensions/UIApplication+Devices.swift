5992d03e62f00c82bc9b38f3bbd1721895ba80f9
DKHT <thevien01@gmail.com>
2018-06-21 21:58:03 +0700

    init project and add UIApplication extension file

---
+++ Example_resources_ios_project/Example_resources_ios_project/Extensions/UIApplication+Devices.swift
@@ -0,0 +1,39 @@
+//
+//  UIApplication+Devices.swift
+//  Example_resources_ios_project
+//
+//  Created by DKHT on 6/20/18.
+//  Copyright © 2018 DKHT. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIApplication{
+    
+//    Check version
+//    Usage:
+//    if iOS_VERSION_GREATER_THAN_OR_EQUAL_TO(version: "11.0") {
+//    Do something!
+//    }
+
+    static func iOS_VERSION_EQUAL_TO(version: String) -> Bool {
+        return UIDevice.current.systemVersion.compare(version, options: NSString.CompareOptions.numeric) == ComparisonResult.orderedSame
+    }
+    
+    static func iOS_VERSION_GREATER_THAN(version: String) -> Bool {
+        return UIDevice.current.systemVersion.compare(version, options: NSString.CompareOptions.numeric) == ComparisonResult.orderedDescending
+    }
+    
+    static func iOS_VERSION_GREATER_THAN_OR_EQUAL_TO(version: String) -> Bool {
+        return UIDevice.current.systemVersion.compare(version, options: NSString.CompareOptions.numeric) != ComparisonResult.orderedAscending
+    }
+    
+    static func iOS_VERSION_LESS_THAN(version: String) -> Bool {
+        return UIDevice.current.systemVersion.compare(version, options: NSString.CompareOptions.numeric) == ComparisonResult.orderedAscending
+    }
+    
+    static func iOS_VERSION_LESS_THAN_OR_EQUAL_TO(version: String) -> Bool {
+        return UIDevice.current.systemVersion.compare(version, options: NSString.CompareOptions.numeric) != ComparisonResult.orderedDescending
+    }
+}
